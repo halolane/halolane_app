0fc2c77cac21bfee31127abc7fece291c026f32a
halolane <myhalolane@gmail.com>
2013-01-14 17:48:32 -0500

    Initial check in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HalolaneApp::Application.config.secret_token = 'db1f38dbaecf931f8946baa9f2a80f190ed15c64ef53e04502847174d627ad1f547ae1d6682e96bb3a5343788667b6ed9365c05c05d0f6348afdf66a5a283478'
