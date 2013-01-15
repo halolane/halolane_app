4cfd8eedf4e6f5770b96301be5a9b7541e99d3a7
halolane <myhalolane@gmail.com>
2013-01-15 18:33:34 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 HalolaneApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
+  get "users/new"
+
+  root to: 'static_pages#home'
+  match '/about', to: 'static_pages#about'
+  match '/signup', to: 'users#new'
   resources :memories
   resources :users
 
