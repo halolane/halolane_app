68653d05b38af54202b24a07af2ae37157f363ca
halolane <myhalolane@gmail.com>
2013-01-15 19:25:23 -0500

    Make a basic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130115000603) do
+ActiveRecord::Schema.define(:version => 20130116001721) do
 
   create_table "memories", :force => true do |t|
     t.string   "content"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(:version => 20130115000603) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
--- test/unit/memory_test.rb
+++ test/unit/memory_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: memories
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class MemoryTest < ActiveSupport::TestCase
