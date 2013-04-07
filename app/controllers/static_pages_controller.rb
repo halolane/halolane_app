3b41a1b22b80b03d71cf731b7c10343805f697bb
halolane <myhalolane@gmail.com>
2013-04-07 15:57:46 -0400

    Updated front page to leather bound storybook

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   def home
   	@user = User.new
   	@profile = Profile.new
+    render :layout => "home_layout"
+    
   	if signed_in? 
   		@user = current_user
   		@profiles = @user.profiles_with_relationships.paginate(page: params[:page])
