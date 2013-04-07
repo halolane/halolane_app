4088f66444edc033b9b7445f2d61ff2c324136c9
halolane <myhalolane@gmail.com>
2013-04-07 09:52:07 -0400

    Changed the title from HaloLane to FamilyTales for each of the pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis
   def full_title(page_title)
-  	base_title = "HaloLane App"
+  	base_title = "FamilyTales"
 
   	if page_title.empty?
   		base_title
