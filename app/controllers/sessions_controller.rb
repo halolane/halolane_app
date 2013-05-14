5626ddded4e2f6971f58010b39a83817fda6f4cc
halolane <myhalolane@gmail.com>
2013-05-14 11:10:23 -0400

    Fixed a bug with /login /signup and also added a loading spinner icon for create a new user so that the user doesn't click it twice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-    user = User.find_by_email(params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
+    @user = User.find_by_email(params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      sign_in @user
       redirect_back_or root_url
     else
       flash.now[:error] = 'Invalid email/password combination'
