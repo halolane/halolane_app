83674617c4c064c8341052a08328f0ab19277f60
halolane <myhalolane@gmail.com>
2013-05-19 16:40:09 -0400

    Fixed bug with user_mailer.rb - I forgot from:

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default "\"FamilyTales\" <hello@familytales.co>"
+  default from: "\"FamilyTales\" <hello@familytales.co>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
