4d77accd06a755888717359ce443ec91063de107
halolane <myhalolane@gmail.com>
2013-05-28 19:47:58 -0400

    added changes to s3 setup for paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,5 +67,13 @@ HalolaneApp::Application.configure do
 
   config.action_mailer.default_url_options = { :host => "www.familytales.co" }
 
-  
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_protocol => 'http',
+    :s3_credentials => {
+      :bucket => ENV['familytales-prod'],
+      :access_key_id => ENV['AKIAJ3AJTP2LJGWA6YPQ'],
+      :secret_access_key => ENV['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']
+    }
+  }
 end
