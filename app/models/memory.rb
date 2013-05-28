c86e1d2971a5c82f2fda406f32829d2f0ea5a119
halolane <myhalolane@gmail.com>
2013-05-28 18:19:18 -0400

    fix for aws

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -12,7 +12,10 @@
 class Memory < ActiveRecord::Base
   
   attr_accessible :content, :profile_id, :photo
-  has_attached_file :photo
+  has_attached_file :photo, 
+        :storage => :s3, 
+        :s3_credentials => "config/s3.yml"
+
 
   belongs_to :user
   belongs_to :profile
