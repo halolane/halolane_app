9974f150f8207129ede0070c621d78b959c02008
halolane <myhalolane@gmail.com>
2013-05-28 20:03:17 -0400

    set config vars for aws

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,8 +2,8 @@ Paperclip::Attachment.default_options.merge!(
 	:storage => :s3,
 	:s3_protocol => 'http',
 	:s3_credentials => {
-		:bucket => ENV['familytales-prod'],
-		:access_key_id => ENV['AKIAJ3AJTP2LJGWA6YPQ'],
-		:secret_access_key => ENV['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']
-	}
+	    :bucket => ENV['AWS_BUCKET'],
+	    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+	    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+	  }
 )
