17d8ffd25247fe9079ef0c74aadd8080babc7171
halolane <myhalolane@gmail.com>
2013-05-29 12:53:16 -0400

    added config/initializers/paperclip.rb

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,13 @@
+Paperclip::Attachment.default_options.merge!(
+	:storage => :s3,
+	:s3_protocol => 'http',
+	:url => ":s3_domain_url",
+	:path => '/:class/:attachment/:id_partition/:style/:filename',
+	:s3_credentials => {
+	    :bucket => ENV['AWS_BUCKET'],
+	    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+	    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+	  }
+)
+
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
