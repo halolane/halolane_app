dd6cadeaf817e08e36246f0062cc8de53376a714
rsolidum <rsolidum@gmail.com>
2013-06-27 22:46:21 -0400

    set config assets and intialize_on_precompile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ HalolaneApp::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.assets.enabled = true
+  config.assets.initialize_on_precompile = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
